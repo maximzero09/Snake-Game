3d36c4b7e1d4efe3222ad613c1e6138b23709b7e
Student <maximzero09@outlook.com>
2026-05-05 10:17:35 +0300

    added logic for moevemnt and score icrementation but "apples" do not respawn,gotta add that logic later.

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ class Program
     static void Main(string[] args)
     {
         ActionState state = ActionState.Idle;
-
+        int score = 0;
         Random rand = new Random();
         int[,] matrix = new int[5, 5];
         int rows = matrix.GetLength(0);
@@ -30,7 +30,7 @@ class Program
 
         while (true)
         {
-            if(Console.KeyAvailable)
+            if (Console.KeyAvailable)
             {
                 var key = Console.ReadKey(true).Key;
                 switch (key)
@@ -47,28 +47,82 @@ class Program
                     case ConsoleKey.D:
                         state = ActionState.Right;
                         break;
-                    
+
                 }
             }
-            System.Threading.Thread.Sleep(50);
+            switch (state)
+            {
+                case ActionState.Up:
+                    if (randomRow > 0)
+                    {
+                        if (matrix[randomRow, randomCol] == 5)
+                        {
+                            score++;
+                        }
+                            randomRow--;
+                        matrix[randomRow, randomCol] = 1; // Move up
+                    }
+                    break;
+                case ActionState.Down:
+                    if (randomRow < rows - 1)
+                    {
+                        if (matrix[randomRow, randomCol] == 5)
+                        {
+                            score++;
+                        }
+                        matrix[randomRow, randomCol] = 0; // Clear current position
+                        randomRow++;
+                        matrix[randomRow, randomCol] = 1; // Move down
+                    }
+                    break;
+                case ActionState.Left:
+                    if (randomCol > 0)
+                    {
+                        if (matrix[randomRow, randomCol] == 5)
+                        {
+                            score++;
+                        }
+                        matrix[randomRow, randomCol] = 0; // Clear current position
+                        randomCol--;
+                        matrix[randomRow, randomCol] = 1; // Move left
+                    }
+                    break;
+                case ActionState.Right:
+                    if (randomCol < cols - 1)
+                    {
+                        if (matrix[randomRow, randomCol] == 5)
+                        {
+                            score++;
+                        }
+                        matrix[randomRow, randomCol] = 0; // Clear current position
+                        randomCol++;
+                        matrix[randomRow, randomCol] = 1; // Move right
+                    }
+                    break;
+            }
+
+
+
+
             // Top border
             Console.WriteLine("+" + new string('-', cols * 4) + "+");
 
-            for (int i = 0; i < rows; i++)
-            {
-                Console.Write("|");
-                Console.WriteLine();
+                    for (int i = 0; i < rows; i++)
+                    {
+                        Console.Write("|");
+                        Console.WriteLine();
 
-                for (int j = 0; j < cols; j++)
-                {
-                    Console.Write($" {matrix[i, j],2} "); // spacing for alignment
-                }
+                        for (int j = 0; j < cols; j++)
+                        {
+                            Console.Write($" {matrix[i, j],2} "); // spacing for alignment
+                        }
 
-                Console.WriteLine("|");
+                        Console.WriteLine("|");
+                    }
+                    Console.WriteLine("+" + new string('-', cols * 4) + "+"); //bottom border
+                    System.Threading.Thread.Sleep(100);
+                    Console.Clear();
             }
-            Console.WriteLine("+" + new string('-', cols * 4) + "+"); //bottom border
-            System.Threading.Thread.Sleep(100); 
-            Console.Clear();
         }
     }
-}
+
